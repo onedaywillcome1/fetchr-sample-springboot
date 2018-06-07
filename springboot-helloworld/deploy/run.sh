45f26235939347afa2a0c1a468f011af59772a13
Ahmet Atalay <ahmet.atalay@siemens.com>
2018-06-08 00:58:33 +0300

    fixed deploy path

--- springboot-helloworld/deploy/run.sh
+++ springboot-helloworld/deploy/run.sh
@@ -3,14 +3,14 @@
 echo "Service is being deployed"
 
 if [ "$1" == "deploy" ];then
-  cp target/us.fetchr.sample*.war deploy/us.fetchr.sample.war
-  cd deploy
+  cp $PWD/fetchr-sample-springboot/springboot-helloworld/target/us.fetchr.sample*.war $PWD/fetchr-sample-springboot/springboot-helloworld/deploy/us.fetchr.sample.war
+  cd $PWD/fetchr-sample-springboot/springboot-helloworld/deploy
   docker-compose kill
   docker-compose build
   docker-compose up -d
 elif [ "$1" == "publish" ];then
   dockerImageId=`docker images | grep deploy_web | grep latest | awk -F " " '{print $3}'`
-  docker tag deploy_web onedaywillcome/fetchr-sample:latest
+  docker tag deploy_web onedaywillcome/fetchr-sample-springboot:latest
   docker login -u $DOCKER_USER -p $DOCKER_PASS
   docker push onedaywillcome/fetchr-sample:latest
 fi
